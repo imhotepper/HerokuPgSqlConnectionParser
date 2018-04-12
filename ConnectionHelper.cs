936224bd1cef0bebca340739841234a227b750f7
daiot <stefanescu.dragos@gmail.com>
2018-04-12 23:21:45 +0300

    fix returned connection string

--- ConnectionHelper.cs
+++ ConnectionHelper.cs
@@ -8,7 +8,7 @@ namespace HerokuPGParser
         {
             Uri url;
             var connectionUrl = "";
-            bool isUrl = Uri.TryCreate("postgres://1user:1password@dbserver.com:4568/testdb", UriKind.Absolute, out url);
+            bool isUrl = Uri.TryCreate(herokuPGConnectionString, UriKind.Absolute, out url);
             if (isUrl)
             {
                 connectionUrl = $"host={url.Host};username={url.UserInfo.Split(':')[0]};password={url.UserInfo.Split(':')[1]};database={url.LocalPath.Substring(1)};pooling=true;";               
