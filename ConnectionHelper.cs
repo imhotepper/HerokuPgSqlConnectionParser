53d1a76edcd30f336e81048753d7b42824bc34ee
daiot <stefanescu.dragos@gmail.com>
2018-04-12 17:40:17 +0300

    initial commit no tests

---
+++ ConnectionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HerokuPGParser
+{
+    public class ConnectionHelper
+    {
+        public static string BuildExpectedConnectionString(string herokuPGConnectionString)
+        {
+            Uri url;
+            var connectionUrl = "";
+            bool isUrl = Uri.TryCreate("postgres://1user:1password@dbserver.com:4568/testdb", UriKind.Absolute, out url);
+            if (isUrl)
+            {
+                connectionUrl = $"host={url.Host};username={url.UserInfo.Split(':')[0]};password={url.UserInfo.Split(':')[1]};database={url.LocalPath.Substring(1)};pooling=true;";               
+            }
+            return connectionUrl;
+        }
+    }
+}
