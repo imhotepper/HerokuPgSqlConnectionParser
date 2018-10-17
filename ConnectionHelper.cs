eeb75cbb10cd31c59b597340b4203516a1fa0eaf
daiot <stefanescu.dragos@gmail.com>
2018-10-17 14:58:21 +0300

    updated to netcoreapp2.1

--- ConnectionHelper.cs
+++ ConnectionHelper.cs
@@ -10,9 +10,7 @@ namespace HerokuPGParser
             var connectionUrl = "";
             bool isUrl = Uri.TryCreate(herokuPGConnectionString, UriKind.Absolute, out url);
             if (isUrl)
-            {
                 connectionUrl = $"host={url.Host};username={url.UserInfo.Split(':')[0]};password={url.UserInfo.Split(':')[1]};database={url.LocalPath.Substring(1)};pooling=true;";               
-            }
             return connectionUrl;
         }
     }
